5c1d66b5bde1f82eb8a4b22e5bb3575c8659f7f7
bvsn2405 <127843818+bvsn2405@users.noreply.github.com>
2023-04-05 14:45:08 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,15 @@
+jsjfdfkvf ;fhvaffa
+dsahfsadj;f
+ sadjfjdas;fh
+  a sdfdpif'dsf
+  a fds
+  af dsofi4asdfdsaofds
+  f as
+  sd fodsf dsa'f
+  as fdsa
+  ofsad
+  f as
+  df 
+  dsaf0ds
+  af idsofds
+  afa
